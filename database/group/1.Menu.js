84461485cce961de3c2ec328b9a9804f034f53d1
ArdiGansYT <78580532+ArdiGansYT@users.noreply.github.com>
2021-02-06 15:59:57 +0700

    Add files via upload

---
+++ database/group/1.Menu.js
@@ -0,0 +1,8 @@
+Hayooo mau ngapain kesini,? ngak ada apa2 browh🤭🤣
+Selamat anda kena prank😂🐗
+ini cuma file prank bagi yg mau editmenunya..
+IZIN DULU BANG SAMA GUA🐗👽
+WA = 085282996146
+            👆salin aja di sv juga lah
+YouTube = ArdiGans YT
+             👆 Cari aja🐗
---
+++ database/group/2.owner.js
@@ -0,0 +1,4 @@
+Kena prank lagi🤦🐗🤣
+Bro² mau edit ya... tu ada di menu lib
+tapi harus izin dulu lah
+Chat wa : 0852829961460
